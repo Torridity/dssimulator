e30ed049e5335171bc58c90ae1d3e9cb40231186
torridity.de <torridity.de@1c10803a-7640-11de-8480-e142f2bfdb3b>
2009-07-27 13:46:37 +0000

    Almost Version 0.95b

--- src/de/tor/tribes/dssim/Constants.java
+++ src/de/tor/tribes/dssim/Constants.java
@@ -12,10 +12,10 @@ import java.awt.Color;
  */
 public class Constants {
 
-    public final static double VERSION = 1.0;
-    public final static String VERSION_ADDITION = "";
+    public final static double VERSION = 0.9;
+    public final static String VERSION_ADDITION = "b";
     public final static Color DS_BACK = new Color(225, 213, 190);
     public final static Color DS_BACK_LIGHT = new Color(239, 235, 223);
-    public final static Color WINNER_GREEN = new Color(34, 139, 34);
+    public final static Color WINNER_GREEN = new Color(96, 238, 90);
     public final static Color LOSER_RED = new Color(255, 99, 71);
 }
